97f16354ee711c8095241a4b7c012b96f4382712
Venkatesh Arjunan <venkata6@yahoo.com>
2025-10-11 12:42:47 -0700

    hw 2

--- homework2/homework/autoregressive.py
+++ homework2/homework/autoregressive.py
@@ -5,10 +5,15 @@ import torch
 def load() -> torch.nn.Module:
     from pathlib import Path
 
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
     model_name = "AutoregressiveModel"
     model_path = Path(__file__).parent / f"{model_name}.pth"
     print(f"Loading {model_name} from {model_path}")
-    return torch.load(model_path, weights_only=False)
+    return torch.load(model_path,map_location=device, weights_only=False)
 
 
 class Autoregressive(abc.ABC):
--- homework2/homework/bsq.py
+++ homework2/homework/bsq.py
@@ -8,10 +8,16 @@ from .ae import PatchAutoEncoder
 def load() -> torch.nn.Module:
     from pathlib import Path
 
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
     model_name = "BSQPatchAutoEncoder"
     model_path = Path(__file__).parent / f"{model_name}.pth"
     print(f"Loading {model_name} from {model_path}")
-    return torch.load(model_path, weights_only=False)
+    return torch.load(model_path,map_location=device, weights_only=False)
+
 
 
 def diff_sign(x: torch.Tensor) -> torch.Tensor:
