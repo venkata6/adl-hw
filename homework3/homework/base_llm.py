97ef677286cf10ad693e0e1f870acc030957461b
Venkatesh Arjunan <venkata6@yahoo.com>
2025-11-01 15:23:44 -0700

    hw3

--- homework3/homework/base_llm.py
+++ homework3/homework/base_llm.py
@@ -142,7 +142,8 @@ class BaseLLM:
         outputs = self.model.generate(
             inputs["input_ids"],
             attention_mask=inputs["attention_mask"],
-            max_new_tokens=150,
+            max_new_tokens=50,
+            no_repeat_ngram_size=3,
             do_sample=do_sample,
             temperature=temperature if do_sample else 1.0,
             num_return_sequences=actual_num_return,
