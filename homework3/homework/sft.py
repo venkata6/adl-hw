5f5093c8ac2a86854e3e1932eba162737f20cc82
Venkatesh Arjunan <venkata6@yahoo.com>
2025-10-31 22:58:06 -0700

    rft

--- homework3/homework/sft.py
+++ homework3/homework/sft.py
@@ -211,7 +211,7 @@ def train_model(
         num_train_epochs=5,
         per_device_train_batch_size=32,
         gradient_checkpointing=True,  # Save GPU memory
-        learning_rate=3e-4,
+        learning_rate=5e-5,
         weight_decay=0.01,
         logging_steps=10,
         save_strategy="epoch",
